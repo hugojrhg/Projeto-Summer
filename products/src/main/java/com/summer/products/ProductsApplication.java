736e2da17a550d5552afb74b0ca8402acdd28f87
Hugo <hugoeafreitas@fac.pe.senac.br>
2022-03-01 13:35:41 -0300

    New Product-Service and Service-Discovery

--- products/src/main/java/com/summer/products/ProductsApplication.java
+++ products/src/main/java/com/summer/products/ProductsApplication.java
@@ -2,8 +2,10 @@ package com.summer.products;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ProductsApplication {
 
 	public static void main(String[] args) {
---
+++ products/src/main/java/com/summer/products/enums/Tipos.java
@@ -0,0 +1,10 @@
+package com.summer.products.enums;
+
+public enum Tipos {
+
+	PARTE_BAIXO,
+	PARTE_CIMA,
+	ACESSORIO,
+	CORPO_INTEIRO,
+	
+}
